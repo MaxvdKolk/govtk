df3eea7464ef693e112a6eb6702cff5f5fbfe20c
max <m.vanderkolk@tudelft.nl>
2020-03-12 09:39:08 +0100

    Extended encoder tests.
    The tests for float64 slices now have reference values for the byte
    representation as well as uncompressed and compressed base64 encoded
    results. For the different encoders and compressors, the resulting byte
    slice of the encode function is compared with the provided, expected
    solutions.

--- encoder_test.go
+++ encoder_test.go
@@ -3,66 +3,178 @@ package vtu
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 	"testing"
 )
 
 type pair struct {
-	val []float64
-	str []byte
+	val       []float64
+	str       []byte
+	hex       string
+	b64, b64c []byte
 }
 
-// Verify binarise creates the expected bytes of some input data.
-// todo: test more inputs?
-func TestBinarise(t *testing.T) {
-	var pairs = []pair{
-		pair{
-			val: []float64{1},
-			str: []byte("1.000000"),
-		},
-		pair{ // print -, dont print +
-			val: []float64{-1, +1},
-			str: []byte("-1.000000 1.000000"),
-		},
-		pair{ // space before -
-			val: []float64{1, -1},
-			str: []byte("1.000000 -1.000000"),
-		},
-		pair{
-			val: []float64{1, 2, 3},
-			str: []byte("1.000000 2.000000 3.000000"),
-		},
-		pair{
-			val: make([]float64, 3),
-			str: []byte("0.000000 0.000000 0.000000"),
-		},
-	}
+// the test values are only for float64 now...
+// probably better to extend to more elaborate examples
+var pairs = []pair{
+	pair{
+		val:  []float64{1},
+		str:  []byte("1.000000"),
+		hex:  "000000000000f03f",
+		b64:  []byte("CAAAAAAAAAAAAPA/"),
+		b64c: []byte("AQAAAAgAAAAIAAAAEQAAAA==eJxiAIMP9oAAAAD//wInATA="),
+	},
+	pair{ // print -, dont print +
+		val:  []float64{-1, +1},
+		str:  []byte("-1.000000 1.000000"),
+		hex:  "000000000000f0bf000000000000f03f",
+		b64:  []byte("EAAAAAAAAAAAAPC/AAAAAAAA8D8="),
+		b64c: []byte("AQAAABAAAAAQAAAAEwAAAA==eJxiAIMP+6G0PSAAAP//EkYC3w=="),
+	},
+	pair{ // space before -
+		val:  []float64{1, -1},
+		str:  []byte("1.000000 -1.000000"),
+		hex:  "000000000000f03f000000000000f0bf",
+		b64:  []byte("EAAAAAAAAAAAAPA/AAAAAAAA8L8="),
+		b64c: []byte("AQAAABAAAAAQAAAAEwAAAA==eJxiAIMP9lB6PyAAAP//DkYC3w=="),
+	},
+	pair{
+		val:  []float64{1, 2, 3},
+		str:  []byte("1.000000 2.000000 3.000000"),
+		hex:  "000000000000f03f00000000000000400000000000000840",
+		b64:  []byte("GAAAAAAAAAAAAPA/AAAAAAAAAEAAAAAAAAAIQA=="),
+		b64c: []byte("AQAAABgAAAAYAAAAGAAAAA==eJxiAIMP9hCawQFCcTgAAgAA//8XtwG4"),
+	},
+	pair{
+		val:  make([]float64, 3),
+		str:  []byte("0.000000 0.000000 0.000000"),
+		hex:  "000000000000000000000000000000000000000000000000",
+		b64:  []byte("GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="),
+		b64c: []byte("AQAAABgAAAAYAAAADwAAAA==eJxiwAEAAQAA//8AGAAB"),
+	},
+}
 
-	// ascii requires manual desired output
-	t.Logf("Ascii encoder binarise")
+// Ensure the content of byte slices are expected for each encoding format.
+func TestBinarise(t *testing.T) {
+
+	// ascii format
 	enc := Asciier{}
+	buf := new(bytes.Buffer)
 	for _, pair := range pairs {
+		buf.Reset()
+		n := int32(len(pair.str))
+		err := binary.Write(buf, binary.LittleEndian, n)
+		if err != nil {
+			t.Fatalf("Cannot setup reference values %v.", err)
+		}
+
 		p := enc.binarise(pair.val)
-		if !bytes.Equal(p.body.Bytes(), pair.str) {
-			t.Errorf("Wrong binarised content for %v", pair.val)
+
+		// header content
+		got := p.head.Bytes()
+		exp := buf.Bytes()
+		if !bytes.Equal(got, exp) {
+			t.Errorf("Wrong header: got: %x exp: %x", got, exp)
+		}
+
+		// body content
+		got = p.body.Bytes()
+		exp = pair.str
+		if !bytes.Equal(got, exp) {
+			t.Errorf("Wrong body: got: %x exp: %x", got, exp)
 		}
 	}
 
-	// base64 and binary just validate wrt a binary.writer
+	// base64, binary format
 	encoders := []encoder{Base64er{}, Binaryer{}}
 	for _, enc := range encoders {
-		r := newPayload()
 		for _, pair := range pairs {
 			p := enc.binarise(pair.val)
-			r.reset()
-			err := binary.Write(r.body, binary.LittleEndian, pair.val)
+
+			buf.Reset()
+			n := int32(len(pair.val) * 8)
+			err := binary.Write(buf, binary.LittleEndian, n)
 			if err != nil {
-				t.Error("Failed setting up buffers.")
+				t.Fatalf("Cannot setup reference values %v.", err)
 			}
 
-			if !bytes.Equal(p.body.Bytes(), r.body.Bytes()) {
-				t.Errorf("Wrong binarised content for enc: %T, data: %v",
-					enc, pair.val)
+			// header content
+			got := p.head.Bytes()
+			exp := buf.Bytes()
+			if !bytes.Equal(got, exp) {
+				t.Errorf("Wrong header: got %x exp %x", got, exp)
 			}
+
+			// body content
+			decoded, err := hex.DecodeString(pair.hex)
+			if err != nil {
+				t.Fatalf("Cannot decode hex: %v", pair.hex)
+			}
+			exp = p.body.Bytes()
+			if !bytes.Equal(decoded, exp) {
+				t.Logf("hello %08x", p.body.Bytes())
+				t.Errorf("Wrong header: got %x exp %x", decoded, exp)
+			}
+		}
+	}
+}
+
+// Ensure ascii encoding only contains the payloads body
+func TestEncodeAscii(t *testing.T) {
+	enc := Asciier{}
+	for _, pair := range pairs {
+		p := enc.binarise(pair.val)
+		b := enc.encode(p)
+		if !bytes.Equal(pair.str, b) {
+			t.Errorf("%T provides wrongly encoded payload", enc)
+		}
+	}
+}
+
+// Ensure the binary encoding just equals the body and header
+// This seems a bit trivial?
+func TestEncodeBinary(t *testing.T) {
+	enc := Binaryer{}
+	compressors := []compressor{noCompression{}, zlibCompression{}}
+
+	for _, c := range compressors {
+		for _, p := range pairs {
+			pl := c.compress(enc.binarise(p.val))
+			got := enc.encode(pl)
+			exp := append(pl.head.Bytes(), pl.body.Bytes()...)
+
+			if !bytes.Equal(got, exp) {
+				t.Errorf("Wrongly encoded payload: got: %x, exp: %x", got, exp)
+			}
+		}
+	}
+}
+
+// Ensure we get expected values.
+// Current tests are quite limited, i.e. only float64 slices.
+func TestEncodeBase64(t *testing.T) {
+	enc := Base64er{}
+	var c compressor
+
+	c = noCompression{}
+	for _, p := range pairs {
+		pl := c.compress(enc.binarise(p.val))
+		got := enc.encode(pl)
+
+		if !bytes.Equal(got, p.b64) {
+			t.Errorf("Wrongly uncompressed base64 encoding: got: %v exp: %v",
+				string(got), string(p.b64))
+		}
+	}
+
+	c = zlibCompression{}
+	for _, p := range pairs {
+		pl := c.compress(enc.binarise(p.val))
+		got := enc.encode(pl)
+
+		if !bytes.Equal(got, p.b64c) {
+			t.Errorf("Wrongly uncompressed base64 encoding: got: %v exp: %v",
+				string(got), string(p.b64))
 		}
 	}
 }
